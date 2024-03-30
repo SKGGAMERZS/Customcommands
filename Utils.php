32d51e8aed4004cf55cd0b38e21115d3f59d344b
SKGGAMERZS <155265985+SKGGAMERZS@users.noreply.github.com>
2024-03-30 19:00:08 +0530

    Create Utils.php

---
+++ Utils.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SKGGAMERZS;
+
+class Utils {
+
+    public static function formatMessage(string $message): string {
+        // Add formatting or processing to the message if needed
+        return "[Plugin] " . $message;
+    }
+}
